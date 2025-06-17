e5db5c3ccb2e4c71d3869a7406c4a90f0c3c7cb9
Anshul12102004 <kumaranshul8445oo@gmail.com>
2025-06-18 03:23:25 +0530

    Delete Chapter 3 directory

--- Chapter 3/2_DataTypes.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-a = np.array([1, 2, 3])
-b = np.array([1, 2, 3], dtype='float32')
-
-print(a.dtype)a = np.array([1, 2, 3])
-print(b)
--- Chapter 3/8_Slicing.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-# b = np.array([0, 1, 2, 3, 4, 5])
-# print(b[1:5:2])
-
-c = np.array([[1, 2, 3], [4, 5, 6]])
-print(c[:, 1:])  # columns 1 and 2
-
